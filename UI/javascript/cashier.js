54a00b16ca9ecf5879188ccdade7214cb838a91e
irfiacre <firaduk@yahoo.com>
2020-02-03 23:19:31 +0200

    ft(cashier account):adding staff(cashier) account - adding a page(html,css,js) of the staff*(cashier)'s account.
    [finishes  #171029558]

---
+++ UI/javascript/cashier.js
@@ -0,0 +1,30 @@
+
+
+const change =()=>{
+    const type1 = document.querySelector('.type1');
+    const type = document.querySelector('.type2');
+    const view1 = document.querySelector('.viewDebit')
+    const view2 = document.querySelector('.viewCredit')
+
+    if(type.className == 'type2' && view2.className=='viewCredit'){
+        view1.className= 'viewDebit2';
+        view2.className= 'viewCredit2';
+        type.setAttribute('id','credit2')
+        type1.setAttribute('id','debit')
+    }
+}
+
+const change2 =()=>{
+    const type1 = document.querySelector('.type1');
+    const view1 = document.querySelector('.viewDebit2')
+    const view2 = document.querySelector('.viewCredit2')
+    const type = document.querySelector('.type2');
+
+    if(type1.className == 'type1' && view1.className=='viewDebit2'){
+        view1.className= 'viewDebit';
+        view2.className= 'viewCredit';
+        type1.setAttribute('id','debit2')
+        type.setAttribute('id','credit')
+    }
+}
+
