4bceff17b0633aad5d830df63851abaeca078824
irfiacre <firaduk@yahoo.com>
2020-02-03 20:27:54 +0200

    ft(user Account):adding the user's account
    - adding a page(html,css,js) of the user's account
    - modifying openAc.html and sign.html to link them to the user's Account
    
    [finishes #171029500]

---
+++ UI/javascript/moodle.js
@@ -0,0 +1,22 @@
+var modal = document.getElementById("modal1");
+var btn = document.getElementById("transaction");
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
